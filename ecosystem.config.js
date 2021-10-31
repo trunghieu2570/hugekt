ebc68d1c26beec0e7b2a4ac1d81aa1c20d94b6a9
Trung Hieu <trunghieu2570@gmail.com>
2021-10-31 20:06:13 +0700

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,8 +22,13 @@ module.exports = {
             ref: 'origin/master',
             repo: 'https://github.com/trunghieu2570/hugekt.git',
             path: '/home/bitnami/hugekt-default',
-            'post-deploy':
-                'source $NVM_DIR/nvm.sh && npm i -g @nestjs/cli && npm i && npm run build && pm2 reload ecosystem.config.js --env production && pm2 save && git checkout yarn.lock',
+            'post-deploy': `source $NVM_DIR/nvm.sh 
+             && npm i -g @nestjs/cli && npm i 
+             && npm run migration:run 
+             && npm run build 
+             && pm2 reload ecosystem.config.js --env production 
+             && pm2 save 
+             && git checkout yarn.lock`,
             env: {
                 NODE_ENV: 'production',
                 NVM_DIR: '$HOME/.nvm',
